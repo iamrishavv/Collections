4c3d5077deb4193acebceae8708fb0507db79f66
Rishav <95118742+iamrishavv@users.noreply.github.com>
2026-01-27 16:47:29 +0530

    Add files via upload

---
+++ queue.java
@@ -0,0 +1,19 @@
+package BasicJava;
+//Queue is a FIFO (First In First Out) data structure.
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class queue {
+    public static void main(String[] args) {
+        //priority queue works on priority of an element
+        Queue<Integer> pq=new PriorityQueue<>();
+        pq.add(1);
+        pq.add(2);
+        pq.add(3);
+        pq.add(4);
+        System.out.println(pq);
+        System.out.println(pq.element());//gives head of the queue
+        System.out.println(pq.peek());
+    }
+}
