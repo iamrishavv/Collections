3e6f31644d92e216a79b2a5f470c4dffaa5aabe2
Rishav <95118742+iamrishavv@users.noreply.github.com>
2026-01-28 16:05:32 +0530

    Add files via upload

--- concurrentHashMap.java
+++ concurrentHashMap.java
@@ -6,13 +6,20 @@ import java.util.concurrent.ConcurrentHashMap;
 //👉 Part of java.util.concurrent package
 //👉 Implements ConcurrentMap Interface
 //👉 Best replacement for Hashtable
+//ConcurrentHashMap shows fail-safe behavior and thread-safe access 👍
 public class concurrentHashMap {
     public static void main(String[] args) {
         ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<>();
         map.put(1, "Java");
         map.put(2, "Spring");
         map.put(3, "Microservices");
-        System.out.println(map);
-        System.out.println(map.get(2)); // Spring
+//        System.out.println(map);
+//        System.out.println(map.get(2)); // Spring
+        for(Integer key:map.keySet()){
+            System.out.println(map.get(key));
+            map.put(4,"Docker");
+        }
+        System.out.println("Final map: "+ map);
+
     }
 }
